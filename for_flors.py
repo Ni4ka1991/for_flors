61bf790fb341a7c2eea0c6a831029d6975c4864e
Ni4ka1991 <astahova_nica@mail.ru>
2021-03-28 17:16:06 +0300

    Add file for_flors.py

--- for_flors.py
+++ for_flors.py
@@ -5,8 +5,7 @@ from random import randint
 #DATA
 
 top_flor   = 9
-lift_flor  = 0
-#randint( 0, top_flor )
+lift_flor  = randint( 0, top_flor )
 human_flor = randint( 0, top_flor )
 
 #LOGIC
@@ -15,16 +14,20 @@ print( "  -------" )
 
 for f in range( 0, top_flor + 1 ):
 
-    if( top_flor == 0 ):
+    if(( top_flor == 0 ) and ( top_flor == lift_flor ) and ( top_flor == human_flor )):
+        print( "P", "|L|_H_|" )
+        
+    elif(( top_flor == lift_flor) and ( top_flor == human_flor )):
+        print( top_flor, "|L|_H_|" )
+
+    elif( top_flor == 0 ):
         print( "P", "| |___|" )
-    
-    
-    if( top_flor == lift_flor):
+
+    elif( top_flor == lift_flor ):
         print( top_flor, "|L|___|" )
 
-    
     elif( top_flor == human_flor ):
-        print( top_flor, "| |_H_|" )
+        print(top_flor, "| |_H_|")
 
     
     else:
@@ -35,22 +38,16 @@ for f in range( 0, top_flor + 1 ):
 print( "  -------" )
 
 
+#lift status
 
+if( lift_flor > human_flor ):
+    print( "Lift descending" )
 
+if( lift_flor < human_flor ):
+    print( "Lift ascending" )
 
-
-print( "L = ", lift_flor )
-#print( "H = ", human_flor )
-
-
-#if( lift_flor > human_flor ):
-#    print( "Lift descending" )
-
-#if( lift_flor < human_flor ):
-#    print( "Lift ascending" )
-
-#if( lift_flor == human_flor ):
-#    print( "Lift arrived" )
+if( lift_flor == human_flor ):
+    print( "Lift arrived" )
 
 
 
